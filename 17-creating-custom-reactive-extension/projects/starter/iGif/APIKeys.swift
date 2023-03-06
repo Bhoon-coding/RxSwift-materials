39c2649ab6d1a5afff6cab00210daf7c9f54c7a2
bhoon-coding <bhooncoding@gmail.com>
2023-03-06 09:44:04 +0900

    chore: git ignore 추가 (APIKey)

---
+++ 17-creating-custom-reactive-extension/projects/starter/iGif/APIKeys.swift
@@ -0,0 +1,13 @@
+//
+//  APIKeys.swift
+//  iGif
+//
+//  Created by BH on 2023/03/06.
+//  Copyright © 2023 Ray Wenderlich. All rights reserved.
+//
+
+import Foundation
+
+struct APIKeys {
+    static let giphyAPI: String = "LkXOReXmazkQHsrOGZEa7c6HY5P9tiMy"
+}
